fe5f66bee8ea14e16027ad702a7f155de2d5cb3e
hippli <philip.e.harrison@gmail.com>
2013-08-20 15:24:15 -0400

    fixed fibo_finder

--- fibo_finder.rb
+++ fibo_finder.rb
@@ -1,22 +1,59 @@
-# def fibo_finder(n)
-#   first_num = 0
-#   second_num = 1
+def fibo_finder(n) # 3
+  first_num = 0  # => 0 (1st fibo number)
+  second_num = 1
 
-#   n.times do |i|
-#     first_num, second_num = second_num, first_num + second_num
-#   end
+  # 4.times do
+  #   first_num, second_num = second_num, first_num + second_num # 
+  # end
 
-# end
+  # ------------------- or ------------------------
 
-def fibo_finder(n)
-    first, second= 0, 1
-  
-  n.times {
-      first, second= second, first + second
-  }
-  
-  first
+  n.times do
+    next_first_num = second_num
+    second_num = first_num + second_num
+    first_num = next_first_num
+  end
+
+
+  # ------------------- or ------------------------
+
+  # # First Iteration
+  # next_first_num = second_num # => 1
+  # second_num = first_num + second_num # => 1
+  # first_num = next_first_num # => 1 (2nd fibo number)
+
+  # # Second Iteration
+  # next_first_num = second_num # => 1
+  # second_num = first_num + second_num # => 2
+  # first_num = next_first_num # => 1 (3rd fibo number)
+
+  # # Third Iteration (5th fibo number)
+  # next_first_num = second_num # => 2
+  # second_num = first_num + second_num # => 3
+  # first_num = next_first_num # => 2 (4th fibo number)
+
+  # # Third Iteration (6th fibo number)
+  # next_first_num = second_num # => 3
+  # second_num = first_num + second_num # => 5
+  # first_num = next_first_num # => 3 (5th fibo number)
+
+  return first_num
+
 end
+
+
+
+
+
+# def fibo_finder(n)
+#     first, second= 0, 1
+  
+#   n.times {
+#       first, second= second, first + second
+#   }
+  
+#   first
+# end
 
 puts fibo_finder(0)
 puts fibo_finder(1)
---
+++ happy_num.rb
@@ -0,0 +1,9 @@
+def happy_number(n)
+  y = n * n
+  z = y + y
+  if condition
+     Math.sqrt(z) == 0
+  end
+end
+
+puts happy_number(7)
